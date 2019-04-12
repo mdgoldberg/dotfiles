2e8092891645ffd9df8567b61dd659b54fb0b951
Matt Goldberg <matt.goldberg7@gmail.com>
2019-04-12 14:56:42 -0700

    Updates

--- vim/config/plugin_config.vim
+++ vim/config/plugin_config.vim
@@ -60,12 +60,13 @@ let g:ale_completion_enabled = 0
 let g:ale_lint_on_enter = 1
 let g:ale_lint_on_save = 1
 let g:ale_lint_on_text_changed = 'normal'
-let g:ale_open_list = 1
 let g:ale_echo_msg_format = '[%linter%] %s% [code]%'
 
 " Ale language settings
 let g:ale_linters = {}
 let g:ale_fixers = {}
+
+let g:ale_fixers['*'] = ['remove_trailing_lines', 'trim_whitespace']
 
 let g:ale_linters['python'] = ['flake8']
 let g:ale_fixers['python'] = ['remove_trailing_lines', 'trim_whitespace', 'isort', 'yapf']
--- zsh/lazy_load_node.zsh
+++ zsh/lazy_load_node.zsh
@@ -1,20 +1,28 @@
+lazynvm() {
+  unset -f nvm node npm npx
+  export NVM_DIR=~/.nvm
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  if [ -f "$NVM_DIR/bash_completion" ]; then
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+  fi
+}
+
 nvm() {
-    unset -f nvm
-    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
-    nvm "$@"
+  lazynvm
+  nvm $@
 }
 
 node() {
-    unset -f node
-    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
-    node "$@"
+  lazynvm
+  node $@
 }
 
 npm() {
-    unset -f npm
-    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
-    npm "$@"
+  lazynvm
+  npm $@
+}
+
+npx() {
+  lazynvm
+  npx $@
 }
