a355277f566b2a3a316ae9dee7b730684b5e3035
Matt Goldberg <matt.goldberg7@gmail.com>
2024-05-26 22:24:45 -0700

    feat: Use nvim-tmux-navigation instead of vim-tmux-navigator

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -92,7 +92,20 @@ return require("packer").startup(
         -- general
         use { 'numToStr/Comment.nvim', config = function() require('Comment').setup() end }
         use { "folke/which-key.nvim", config = function() require("which-key").setup() end } -- remembering key bindings
-        use "christoomey/vim-tmux-navigator"
+        use {"alexghergh/nvim-tmux-navigation", config = function()
+            require'nvim-tmux-navigation'.setup {
+                disable_when_zoomed = true, -- defaults to false
+                keybindings = {
+                    left = "<C-h>",
+                    down = "<C-j>",
+                    up = "<C-k>",
+                    right = "<C-l>",
+                    last_active = "<C-\\>",
+                    next = "<C-Space>",
+                }
+            }
+        end
+        }
         use "tpope/vim-unimpaired"
         use { "kylechui/nvim-surround", config = function() require("nvim-surround").setup() end }
         use "tpope/vim-repeat"
