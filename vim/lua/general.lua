6e5ea104f4238f205ae70e99696add78614f25e8
Matt Goldberg <matthew.goldberg@geniussports.com>
2025-04-14 19:13:13 -0700

    Various updates before fully porting to lazyvim

--- vim/lua/general.lua
+++ vim/lua/general.lua
@@ -1,7 +1,7 @@
 vim.o.updatetime = 500
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- keep at least 3 lines on either side of cursor in window
 vim.o.scrolloff = 3
@@ -18,15 +18,15 @@ vim.o.hlsearch = false
 
 -- checks if terminal has 24-bit color support
 if vim.fn.has("termguicolors") == 1 then
-    vim.o.termguicolors = true
-    vim.cmd([[hi LineNr ctermbg=NONE guibg=NONE]])
+	vim.o.termguicolors = true
+	vim.cmd([[hi LineNr ctermbg=NONE guibg=NONE]])
 end
 
 -- resize windows when tmux pane size changes
-vim.cmd [[autocmd VimResized * wincmd =]]
+vim.cmd([[autocmd VimResized * wincmd =]])
 
 -- incremental feedback on substitute commands
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 
 -- disable backups
 vim.o.backup = false
@@ -45,22 +45,22 @@ vim.o.expandtab = true
 vim.o.showtabline = 2
 
 -- configs to use rg for :grep
-if vim.fn.executable('rg') then
-    vim.o.grepprg = "rg --vimgrep --no-heading"
-    vim.o.grepformat = "%f:%l:%c:%m,%f:%l:%m"
+if vim.fn.executable("rg") then
+	vim.o.grepprg = "rg --vimgrep --no-heading"
+	vim.o.grepformat = "%f:%l:%c:%m,%f:%l:%m"
 end
 
 -- avoid showing extra message when using completion
-vim.opt.shortmess:append('c')
+vim.opt.shortmess:append("c")
 
 -- terraform
 vim.g.terraform_align = 1
 vim.g.terraform_fmt_on_save = 1
-vim.g.terraform_commentsring = '//%s'
+vim.g.terraform_commentsring = "//%s"
 
 -- fix for gx in netrw (used by :GBrowse)
 -- from: https://github.com/vim/vim/issues/4738#issuecomment-798790444
-vim.cmd [[
+vim.cmd([[
 function! OpenURLUnderCursor()
 	let s:uri = expand('<cWORD>')
 	let s:uri = matchstr(s:uri, "[a-z]*:\/\/[^ >,;)'\"]*")
@@ -72,49 +72,49 @@ function! OpenURLUnderCursor()
 		:redraw!
 	endif
 endfunction
-]]
-vim.keymap.set("n", "gx", ":call OpenURLUnderCursor()<CR>", {noremap = true})
+]])
+vim.keymap.set("n", "gx", ":call OpenURLUnderCursor()<CR>", { noremap = true })
 
-vim.cmd [[
+vim.cmd([[
 " fzf config for hidden files
 command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --follow --line-number --no-heading --color=always --smart-case ".<q-args>, 1, <bang>0)
 let $FZF_DEFAULT_COMMAND = "fd --follow --type file --hidden -E .git"
 let $FZF_DEFAULT_OPTS .= ' --no-height'
-]]
+]])
 
 vim.g.strip_whitespace_on_save = 1
 
 -- colorscheme
 vim.cmd([[colorscheme tokyonight]])
 require("tokyonight").setup({
-  on_highlights = function(hl, c)
-    local prompt = "#2d3149"
-    hl.TelescopeNormal = {
-      bg = c.bg_dark,
-      fg = c.fg_dark,
-    }
-    hl.TelescopeBorder = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopePromptNormal = {
-      bg = prompt,
-    }
-    hl.TelescopePromptBorder = {
-      bg = prompt,
-      fg = prompt,
-    }
-    hl.TelescopePromptTitle = {
-      bg = prompt,
-      fg = prompt,
-    }
-    hl.TelescopePreviewTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopeResultsTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-  end,
+	on_highlights = function(hl, c)
+		local prompt = "#2d3149"
+		hl.TelescopeNormal = {
+			bg = c.bg_dark,
+			fg = c.fg_dark,
+		}
+		hl.TelescopeBorder = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+		hl.TelescopePromptNormal = {
+			bg = prompt,
+		}
+		hl.TelescopePromptBorder = {
+			bg = prompt,
+			fg = prompt,
+		}
+		hl.TelescopePromptTitle = {
+			bg = prompt,
+			fg = prompt,
+		}
+		hl.TelescopePreviewTitle = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+		hl.TelescopeResultsTitle = {
+			bg = c.bg_dark,
+			fg = c.bg_dark,
+		}
+	end,
 })
