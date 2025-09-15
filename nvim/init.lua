ecae4509145ddaafc164326513e1ba412060fab0
Matt Goldberg <matthew.goldberg@geniussports.com>
2025-09-15 11:18:44 -0400

    fix: Skip LazyVim for vscode for now

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  vim.keymap.set({ "n", "v" }, ";", ":")
+  vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { noremap = true })
+  vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { noremap = true })
+else
+  require("config.lazy")
+end
