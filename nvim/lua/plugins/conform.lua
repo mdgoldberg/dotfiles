7c571d3f10827ba56ec12a04b942250810eeecb5
Matt Goldberg <matthew.goldberg@geniussports.com>
2025-09-15 11:18:11 -0400

    feat: Add sql formatting for postgres dialect

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,9 +1,23 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    formatters = { "pyproject-fmt" },
+    formatters = {
+      -- Add sqlfluff to your formatters table
+      -- and specify the arguments
+      sqlfluff = {
+        args = { "format", "--dialect", "postgres", "-" },
+      },
+      -- Keep your existing formatter
+      pyproject_fmt = {
+        command = "pyproject-fmt",
+      },
+    },
     formatters_by_ft = {
       toml = { "pyproject-fmt" },
+      -- Add a new entry for SQL files
+      sql = { "sqlfluff" },
+      -- Add a new entry for `psql` files
+      psql = { "sqlfluff" },
     },
   },
 }
